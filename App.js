5a4b992c314c7d379e75e1f9da5a630d239fbfd3
Bill Buchanan <bbuchanan@gmail.com>
2019-10-02 16:30:48 -0400

    120. Grid styling and refactoring ... also fixed the broken font loading

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import { useScreens } from "react-native-screens";
@@ -7,8 +7,9 @@ import { useScreens } from "react-native-screens";
 import MealsNavigator from "./navigation/MealsNavigator";
 
 useScreens();
+
 const fetchFonts = () => {
-  Font.loadAsync({
+  return Font.loadAsync({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
   });
@@ -19,7 +20,10 @@ export default function App() {
 
   if (!fontLoaded) {
     return (
-      <AppLoading startAsync={fetchFonts} onFinish={setFontLoaded(true)} />
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
     );
   }
 
