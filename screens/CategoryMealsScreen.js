1c63baa2cf3eb7fcd618aad3fb72e9cf688f87a5
Bill Buchanan <bbuchanan@gmail.com>
2019-10-02 15:16:36 -0400

    119. Default navigation options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Platform } from "react-native";
-
-import Colors from "../constants/Colors";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
 
@@ -26,11 +24,7 @@ CategoryMealScreen.navigationOptions = navigationData => {
   const catId = navigationData.navigation.getParam("categoryId");
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
   return {
-    headerTitle: selectedCategory.title,
-    headerStyle: {
-      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "white"
-    },
-    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+    headerTitle: selectedCategory.title
   };
 };
 
