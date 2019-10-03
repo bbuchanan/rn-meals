fa8e6a9102487593394876ca1693a1731c0d9f5f
Bill Buchanan <bbuchanan@gmail.com>
2019-10-03 09:23:45 -0400

    122. Loading meals for categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, FlatList, StyleSheet, Text } from "react-native";
 
-import { CATEGORIES } from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMealScreen = props => {
+  const catId = props.navigation.getParam("categoryId");
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  const renderMealItem = itemData => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() =>
-          props.navigation.navigate({
-            routeName: "MealDetail"
-          })
-        }
-      ></Button>
-      <Button title="Go back" onPress={() => props.navigation.goBack()} />
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={i => i.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
